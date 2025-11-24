92e00f7f1817a8c2f71e0aff1edd31cbe8b89fc0
Asia4046 <kirito234875@gmail.com>
2025-11-24 20:16:48 +0530

    fix and  optimise already inefficient code

--- pieces.py
+++ pieces.py
@@ -2,6 +2,9 @@ import position as p
 
 # rook logic
 def move_rook(board, pos, move_pos):
+    init_piece_color = get_color(board, pos)
+    final_color = get_color(board, move_pos)
+
     if p.get_piece(board, pos) == "BR" or p.get_piece(board, pos) == "WR":
         r_rk, c_rk = p.get_pos(pos)
         m_r, m_c = p.get_pos(move_pos)
@@ -20,62 +23,53 @@ def move_rook(board, pos, move_pos):
         elif c_rk - m_c == 0 and  m_r - r_rk > 0:
             move_down = True
 
-        if move_right == True:                                                                                                                                                                                                                                            
+        if move_right:
             for i in range(c_rk+1, m_c):
                if board[r_rk][i] != "__" : 
                     print("illegal move")                
                     break
             else:
-                if p.get_piece(board, pos)[0] == "W" and p.get_piece(board, move_pos)[0] == "W":
-                    print("cannot cut own  piece")
-                elif p.get_piece(board, pos)[0] == "B" and p.get_piece(board, move_pos)[0] == "B":
+                if init_piece_color == final_color:
                     print("cannot cut own  piece")
                 else:
                     cut_piece(board, pos, move_pos)        
-        elif move_left == True:                                                                                                                                                                                                                                            
+        elif move_left:
             for i in range(m_c+1, c_rk):                            
                if board[r_rk][i] != "__" : 
                     print("illegal move")
                     break
             else:
-                if p.get_piece(board, pos)[0] == "W" and p.get_piece(board, move_pos)[0] == "W":
-                    print("cannot cut own  piece")
-                elif p.get_piece(board, pos)[0] == "B" and p.get_piece(board, move_pos)[0] == "B":
+                if init_piece_color == final_color:
                     print("cannot cut own  piece")
                 else:
-                    cut_piece(board, pos, move_pos)  
-        elif move_down == True:                                                                                                                                                                                                                                            
+                    cut_piece(board, pos, move_pos)
+        elif move_down:
             for i in range(r_rk+1, m_r):                            
                if board[i][c_rk] != "__" : 
                     print("illegal move")
                     break
             else:
-                if p.get_piece(board, pos)[0] == "W" and p.get_piece(board, move_pos)[0] == "W":
-                    print("cannot cut own  piece")
-                elif p.get_piece(board, pos)[0] == "B" and p.get_piece(board, move_pos)[0] == "B":
+                if init_piece_color == final_color:
                     print("cannot cut own  piece")
                 else:
-                    cut_piece(board, pos, move_pos) 
-        elif move_up == True:                                                                                                                                                                                                                                            
+                    cut_piece(board, pos, move_pos)
+        elif move_up:
             for i in range(m_r+1, r_rk):                            
                if board[i][c_rk] != "__" : 
                     print("illegal move")
                     break
             else:
-                if p.get_piece(board, pos)[0] == "W" and p.get_piece(board, move_pos)[0] == "W":
+                if init_piece_color == final_color:
                     print("cannot cut own  piece")
-                elif p.get_piece(board, pos)[0] == "B" and p.get_piece(board, move_pos)[0] == "B":
-                      print("cannot cut own  piece")
                 else:
                     cut_piece(board, pos, move_pos)
         else:
             print("Cannot move diagonally")
     else:
         print(p.get_piece(board, pos))
-        print("Error: Selected postion does not  contain a rook!!")
+        print("Error: Selected position does not  contain a rook!!")
 
 def get_color(board, pos):
-    color = ""
     piece  = p.get_piece(board, pos)
     if "W" in piece:
         color = "White"
